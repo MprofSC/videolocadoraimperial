36bba58ccdcbe8b6439feb75884550f9f3a76a56
Samuel Bristot Loli <bristot@gmail.com>
2019-01-07 18:12:45 -0200

    Initial application generated by JHipster-5.7.0

--- src/main/webapp/app/entities/item-filme/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './item-filme.service';
-export * from './item-filme-update.component';
-export * from './item-filme-delete-dialog.component';
-export * from './item-filme-detail.component';
-export * from './item-filme.component';
-export * from './item-filme.route';
